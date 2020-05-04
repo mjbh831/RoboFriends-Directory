7adb8544c02dd0531b42aff3115e86be60cd754d
mjbh831 <56654912+mjbh831@users.noreply.github.com>
2020-05-04 15:11:20 -0500

    Updates

---
+++ precache-manifest.4812755523d35120ff5227bb0bd8457b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fec63a4b26b88ba0e404a0a8cb4bed",
+    "url": "/RoboFriends-Directory/index.html"
+  },
+  {
+    "revision": "bf7f61bfe24b55bcb19c",
+    "url": "/RoboFriends-Directory/static/css/2.fd6a09da.chunk.css"
+  },
+  {
+    "revision": "65d4cc0f4c42b099e541",
+    "url": "/RoboFriends-Directory/static/css/main.d0038289.chunk.css"
+  },
+  {
+    "revision": "bf7f61bfe24b55bcb19c",
+    "url": "/RoboFriends-Directory/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends-Directory/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d4cc0f4c42b099e541",
+    "url": "/RoboFriends-Directory/static/js/main.88b27151.chunk.js"
+  },
+  {
+    "revision": "bb533183a01c43cc3a37",
+    "url": "/RoboFriends-Directory/static/js/runtime-main.217e6e9b.js"
+  }
+]);
